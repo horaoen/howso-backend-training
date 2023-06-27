97b6f45e5a2f413362f705f6014013686ac3f37c
horaoen <horaoen@gmail.com>
2023-06-27 15:58:26 +0800

    集成redis

--- src/main/java/cn/howso/backendtraining/controller/TestController.java
+++ src/main/java/cn/howso/backendtraining/controller/TestController.java
@@ -1,10 +1,12 @@
 package cn.howso.backendtraining.controller;
 
+import cn.hutool.db.nosql.redis.RedisDS;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 @RestController
 @RequestMapping("/test")
@@ -15,5 +17,13 @@ public class TestController {
     @Operation(summary = "test1")
     public String test1() {
         return "hello";
+    }
+    
+    @GetMapping("2")
+    @Operation(summary = "redis test")
+    public String test2() {
+        Jedis jedis = RedisDS.create().getJedis();
+        jedis.set("hello", "hello world");
+        return jedis.get("hello");
     }
 }
